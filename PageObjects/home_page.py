90864708da73b2dbca736abac3c77a53d8352df6
“zhouweisong” <zhouweisong@longbeach-inc.com>
2018-04-10 18:11:56 +0800

    添加首页资源信息

--- PageObjects/home_page.py
+++ PageObjects/home_page.py
@@ -2,20 +2,31 @@ from Common.BasePage import BasePage
 from selenium.webdriver.common.by import By
 
 class Home_Page(BasePage):
-    #首页 - tab
-    home_homeTab_locator= "//a[text()='首页']"
-    # 首页 - 我的帐户链接
+    # 首页 - 法币OTC交易(mainNav)
+    home_mainNav_fbDeal_locator= "//div[@class = 'main-nav flex-box flex-direction-row flex-justify-start flex-align-item-start flex-wrap-nowrap']/a[@href = 'https://c2c.58coin.com']"
+    # 首页 - 币币交易(mainNav)
+    home_mainNav_BbDeal_locator= "//div[@class = 'main-nav flex-box flex-direction-row flex-justify-start flex-align-item-start flex-wrap-nowrap']/a[@href = 'https://spot.58coin.com/']"
+    # 首页 - 云算力交易(mainNav)
+    home_mainNav_CCPDeal_locator = "//div[@class = 'main-nav flex-box flex-direction-row flex-justify-start flex-align-item-start flex-wrap-nowrap']/a[@href = 'https://hash.58coin.com/']"
+    # 首页 - 杠杆交易(mainNav)
+    home_mainNav_leverDeal_locator ="//div[@class = 'main-nav flex-box flex-direction-row flex-justify-start flex-align-item-start flex-wrap-nowrap']/a[@href = 'https://www.58coin.com/coming/experience']"
+    # 首页 - 高杠杆交易(mainNav)
+    home_mainNav_highLeverDeal_locator ="//div[@class = 'main-nav flex-box flex-direction-row flex-justify-start flex-align-item-start flex-wrap-nowrap']/a[@href = 'https://www.58coin.com/coming/coming-soon']"
+    # 首页 - 账户昵称
     home_userInfoLink_locator = '//a[@href="/users"]'
-    # 首页 - 抢投标按钮 - 第一个
-    home_firstBidGrab_locator = '//*[text()="抢投标"]'
+    # 首页 - 退出
+    home_esc_locator = "//div[@class = 'common-header flex-box flex-direction-row flex-justify-start flex-align-item-start flex-wrap-nowrap']/a[text()='退出']"
+    # 首页 - 立即购买立即收益
+    home_immediatelyIncome_locator = "//div[@class='carousel']/a[@href = 'https://hash.58coin.com/']"
+    # 首页 - 在线客服
+    home_onlineService_locator = "//span[text()='在线客服']"
 
 
-    # 默认选第一标进行投资
-    def click_firstgrabButton(self):
-        self.find_element(By.XPATH, self.home_firstBidGrab_locator, 30).click()
 
     #获取当前登陆的用户名昵称
     def get_nickname(self):
         return self.find_element(By.XPATH,self.home_userInfoLink_locator,40).text
+
+    #
 
 
